880211db3601cb9a584f8f7421895834b160d28a
Dimas Prakasa <dimas.prakasa@global.ntt>
2024-09-19 12:41:50 +0700

    selamat belajar full stack disini

---
+++ frontend-react/src/main.jsx
@@ -0,0 +1,28 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+//import BrowserRouter dari react router
+import { BrowserRouter } from 'react-router-dom';
+
+//import AuthProvider
+import { AuthProvider } from './context/AuthContext';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <AuthProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AuthProvider>,
+)
